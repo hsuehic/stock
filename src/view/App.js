d5798aeff5ab341795297f613820768aa3024629
xuexiaowei <xuexiaowei@megvii.com>
2017-06-24 17:25:27 +0800

    feat: 修改k线图样式

--- src/view/App.js
+++ src/view/App.js
@@ -274,6 +274,12 @@ class App extends Component {
         });
         var option = {
             backgroundColor: '#404141',
+            grid: [{
+                top: '6px',
+                left: '40px',
+                bottom: '20px',
+                right: '2px'
+            }],
             tooltip: {
                 trigger: 'axis',
                 axisPointer: {
@@ -289,7 +295,23 @@ class App extends Component {
             xAxis: {
                 type: 'category',
                 data: dates,
-                axisLine: { lineStyle: { color: '#8392A5' } }
+                axisLine: {
+                    lineStyle: {
+                        color: '#8392A5'
+                    }
+                },
+                axisTick: {
+                    show: false
+                },
+                axisLabel: {
+                    textStyle: {
+                        align: 'center'
+                    },
+                    inside: false
+                },
+                splitLine: {
+                    show: true
+                }
             },
             yAxis: {
                 scale: true,
@@ -297,31 +319,18 @@ class App extends Component {
                 splitLine: { show: false },
                 textStyle: {
                     color: '#65f1f1'
+                },
+                axisTick: {
+                    show: false
+                },
+                axisLabel: {
+                    inside: false
+                },
+                splitLine: {
+                    show: true
                 }
             },
             dataZoom: [{
-                textStyle: {
-                    color: '#65f1f1'
-                },
-                handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
-                handleSize: '80%',
-                dataBackground: {
-                    areaStyle: {
-                        color: '#8392A5'
-                    },
-                    lineStyle: {
-                        opacity: 0.8,
-                        color: '#8392A5'
-                    }
-                },
-                handleStyle: {
-                    color: '#fff',
-                    shadowBlur: 3,
-                    shadowColor: 'rgba(0, 0, 0, 0.6)',
-                    shadowOffsetX: 2,
-                    shadowOffsetY: 2
-                }
-            }, {
                 type: 'inside'
             }],
             animation: false,
@@ -461,7 +470,7 @@ class App extends Component {
                                 echarts = {echarts}
                                 option = {this.getChartOptions()}
                                 notMerge = { true }
-                                lazyUpdate = { true }
+                                lazyUpdate = { false }
                                 onChartReady = { this.onChartReady.bind(this)}
                             />
                         </div>
